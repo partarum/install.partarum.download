ad70faefa47d46c0ea41a0cd609247fe0195effb
Partarum <email@partarum.de>
2023-01-31 01:39:49 +0100

    update add files

--- index.php
+++ index.php
@@ -1,55 +1,68 @@
 <?php
 
 define("URI", explode("/",$_SERVER["REQUEST_URI"]));
+define("USER_SYSTEM", $_POST["system"] || null);
+define("USER_TOKEN", $_POST["token"] || null);
 
-enum Install {
+enum System {
+
+    case PLESK;
+
+    case KEYHELP;
+
+    case BLANK;
+}
+
+enum InstallType {
+
+    case LOADER;
     case SERVER;
     case WEB;
 
     public function getFile(): string {
         return match($this){
-            Install::SERVER => "installPartarumServer",
-            Install::WEB => "installPartarumWeb"
+            InstallType::LOADER => "installPartarum",
+            InstallType::SERVER => "testsh_server",
+            InstallType::WEB => "testsh_web"
         };
     }
+}
 
-    public function sendFile(): void {
+class Install {
+
+    public static function sendFile(): void {
+
+        $file = (Token::check(USER_TOKEN) !== NULL) ? match(USER_SYSTEM){
+          "Plesk", "Keyhelp" => InstallType::WEB->getFile(),
+          "Blank" => InstallType::SERVER->getFile(),
+          default => InstallType::LOADER->getFile()
+        } : InstallType::LOADER->getFile();
 
         header("Content-Description: File Transfer");
-        header('Content-Type: application/octet-stream');
-        header('Content-Disposition: attachment; filename=installPartarum.sh');
-        header('Expires: 0');
-        header('Cache-Control: must-revalidate');
-        header('Pragma: public');
-        header('Content-Length: ' . filesize($file));
+        header("Content-Type: application/octet-stream");
+        header("Content-Disposition: attachment; filename=$file");
+        header("Expires: 0");
+        header("Cache-Control: must-revalidate");
+        header("Pragma: public");
+        header("Content-Length: " . filesize($file));
         readfile($file);
     }
 }
 
-enum User {
-
-    case REGISTERED;
-
-    case UNREGISTERED;
-}
-
 class Token {
-    static public function check() {
+    static public function check($userToken) {
 
+        // ! Hier USER_TOKEN überprüfen
+
+        return $userToken;
     }
 }
 
 /*
+ *  Ablauf
  *
+ *  Abruf ohne Post - Parameter senden die installPartarum - Datei
+ *  Abruf mit Post - Parameter senden die entsprechende Datei
  */
 
-switch(URI[0]) {
-    case "Plesk":
-    case "plesk":
-    case "Keyhelp":
-    case "keyhelp":
-        Install::WEB->sendFile();
-        break;
-    default:
-        Install::SERVER->sendFile();
-}
+Install::sendFile();
